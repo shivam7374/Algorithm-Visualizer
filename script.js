444f4ce9138802c79f1b4ee17e4e7115b8845dfd
Umair Ayub <46655787+umairayub79@users.noreply.github.com>
2022-10-06 21:06:49 +0500

    Added Input for Array Size

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-var random_array=document.getElementById('radom_array');
-var array_generated=document.getElementById("array");
+var generate_array = document.getElementById('array_generator');
+var array_size_input = document.getElementById('arraySizeInput');
+var random_array = document.getElementById('radom_array');
+var array_generated = document.getElementById("array");
 
-var i=0, j=0,m=0;
-var start,end;//for recording the time 
+var i = 0, j = 0, m = 0;
+var start, end;//for recording the time 
 var globinterval;
 
 
-function random_array_generator(){
-    array_generated.innerHTML="";
-    for(var k=0;k<8;k++)
-    {
-        var val=randomIntFromInterval(10,100);
+function random_array_generator() {
+    array_generated.innerHTML = "";
+    for (var k = 0; k < 8; k++) {
+        var val = randomIntFromInterval(10, 100);
         var node = document.createElement("div");
-        node.setAttribute("class","bar_view");  
-        node.style.height=val*4+"px";     
+        node.setAttribute("class", "bar_view");
+        node.style.height = val * 4 + "px";
         // var textnode = document.createTextNode(val);         
         // node.appendChild(textnode);                         
-        array_generated.appendChild(node);    
+        array_generated.appendChild(node);
     }
-    i=0;
-    j=0;
+    i = 0;
+    j = 0;
 }
 
+function array_generator() {
+    let array_size = array_size_input.value || 10
+    array_generated.innerHTML = "";
+    for (var k = 0; k < array_size; k++) {
+        var val = randomIntFromInterval(10, 100);
+        var node = document.createElement("div");
+        node.setAttribute("class", "bar_view");
+        node.style.height = val * 4 + "px";
+        // var textnode = document.createTextNode(val);         
+        // node.appendChild(textnode);                         
+        array_generated.appendChild(node);
+    }
+    i = 0;
+    j = 0;
+}
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+}
 
