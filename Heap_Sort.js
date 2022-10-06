7c5e437b24f1c77469d1c0cabcb79f3d962b0aa3
Pranjal Agarwal <98935622+Pranjal360Agarwal@users.noreply.github.com>
2022-10-07 01:42:15 +0530

    Create Heap_Sort.js

---
+++ Heap_Sort.js
@@ -0,0 +1,61 @@
+//Heap sort is a comparison-based sorting technique based on Binary Heap data structure. 
+//It is similar to the selection sort where we first find the minimum element and place the minimum element at the beginning. 
+//Repeat the same process for the remaining elements.
+
+
+var array_length;
+/* to create MAX  array */  
+function heap_root(input, i) 
+{
+    var left = 2 * i + 1;
+    var right = 2 * i + 2;
+    var max = i;
+
+    if (left < array_length && input[left] > input[max]) 
+    {
+        max = left;
+    }
+
+    if (right < array_length && input[right] > input[max])   
+    {
+        max = right;
+    }
+
+    if (max != i) 
+    {
+        swap(input, i, max);
+        heap_root(input, max);
+    }
+}
+
+function swap(input, index_A, index_B) 
+{
+    var temp = input[index_A];
+
+    input[index_A] = input[index_B];
+    input[index_B] = temp;
+}
+
+function heapSort(input)
+{
+    
+    array_length = input.length;
+
+    for (var i = Math.floor(array_length / 2); i >= 0; i -= 1)   
+    {
+        heap_root(input, i);
+      }
+
+    for (i = input.length - 1; i > 0; i--) 
+    {
+        swap(input, 0, i);
+        array_length--;
+      
+      
+        heap_root(input, 0);
+    }
+}
+
+var arr = [3, 0, 2, 5, -1, 4, 1];
+heapSort(arr);
+console.log(arr);
