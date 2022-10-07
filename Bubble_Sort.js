874b319b968278062bcc1fc5d33f991b634b546d
niteeshpatil <niteeshgpatil5@gmail.com>
2022-10-07 19:56:29 +0530

    added style

--- Bubble_Sort.js
+++ Bubble_Sort.js
@@ -1,46 +1,40 @@
-function bubbleSort()  
-{  
-    
-    var elements = document.getElementById('array').children; 
-    var n=elements.length; 
-    var element1=elements[j].style;
-    var element2=elements[j+1].style;
-    var val1=parseInt(element1.height);
-    var val2=parseInt(element2.height);
-    element1.backgroundColor ="red";
-    element2.backgroundColor ="red";
+function bubbleSort() {
+  var elements = document.getElementById("array").children;
+  var n = elements.length;
+  var element1 = elements[j].style;
+  var element2 = elements[j + 1].style;
+  var val1 = parseInt(element1.height);
+  var val2 = parseInt(element2.height);
+  element1.backgroundColor = "red";
+  element2.backgroundColor = "red";
 
-    if (val1 > val2)
-    {
-        element1.backgroundColor ="orange";
-        element2.backgroundColor ="orange";
-        element1.height=val2+"px";
-        element2.height=val1+"px";
-    }
+  if (val1 > val2) {
+    element1.backgroundColor = "orange";
+    element2.backgroundColor = "orange";
+    element1.height = val2 + "px";
+    element2.height = val1 + "px";
+  }
 
-    j++;
-    if(j==(n-i-1))
-    {
-        j=0;
-        i++;
+  j++;
+  if (j == n - i - 1) {
+    j = 0;
+    i++;
     // element1.backgroundColor ="aquamarine";
-    element2.backgroundColor ="aquamarine";
-    }
-    if(i==n-1)
-    {
-        var elements = document.getElementById('array').children; 
-        elements[0].style.backgroundColor ="aquamarine"
-        end = window.performance.now();
-        console.log(`Execution time: ${end - start} ms`);
-        // console.log("BOSS");
-        clearInterval(globinterval);
-    }
-}  
+    element2.backgroundColor = "blueviolet";
+  }
+  if (i == n - 1) {
+    var elements = document.getElementById("array").children;
+    elements[0].style.backgroundColor = "blueviolet";
+    end = window.performance.now();
+    console.log(`Execution time: ${end - start} ms`);
+    // console.log("BOSS");
+    clearInterval(globinterval);
+  }
+}
 
-function bubble_sortonclick()
-{
-    start = window.performance.now();
-    i=0,j=0;
-    var startinterval=setInterval(bubbleSort, 1000);
-    globinterval=startinterval;
+function bubble_sortonclick() {
+  start = window.performance.now();
+  (i = 0), (j = 0);
+  var startinterval = setInterval(bubbleSort, 1000);
+  globinterval = startinterval;
 }
--- Insertion_Sort.js
+++ Insertion_Sort.js
@@ -1,44 +1,40 @@
 var key_insertion;
-function insertionSort()  
-{  
-    var elements = document.getElementById('array').children; 
-    var n=elements.length; 
-    var element1=elements[j].style;
-    var element2=elements[j+1].style;
-    var val1=parseInt(element1.height);
+function insertionSort() {
+  var elements = document.getElementById("array").children;
 
-    if (val1 > key_insertion)
-    {
-        element2.backgroundColor ="orange";
-        element2.height=val1+"px";
-        j--;
-    }
-    if(j==-1 || val1 <= key_insertion){
-        j++;
-        elements[j].style.height=key_insertion+"px";
-        i++;
-        if(i==n)
-        {
-            end = window.performance.now();
-            console.log(`Execution time: ${end - start} ms`);
-            clearInterval(globinterval);
-        }
-        else{
-            key_insertion=parseInt(elements[i].style.height);
-            elements[i].style.backgroundColor="purple";
-            j=i-1;
-            element2.backgroundColor ="aquamarine";
-        }
-    }
-} 
+  var n = elements.length;
+  var element1 = elements[j].style;
+  var element2 = elements[j + 1].style;
+  var val1 = parseInt(element1.height);
 
-function insertion_sortonclick()
-{
-    start = window.performance.now();
-    i=1,j=0;
-    var elements = document.getElementById('array').children; 
-    key_insertion=parseInt(elements[i].style.height);
-    elements[i].style.backgroundColor="purple";
-    var startinterval=setInterval(insertionSort, 1000);
-    globinterval=startinterval;
+  if (val1 > key_insertion) {
+    element2.backgroundColor = "orange";
+    element2.height = val1 + "px";
+    j--;
+  }
+  if (j == -1 || val1 <= key_insertion) {
+    j++;
+    elements[j].style.height = key_insertion + "px";
+    i++;
+    if (i == n) {
+      end = window.performance.now();
+      console.log(`Execution time: ${end - start} ms`);
+      clearInterval(globinterval);
+    } else {
+      key_insertion = parseInt(elements[i].style.height);
+      elements[i].style.backgroundColor = "purple";
+      j = i - 1;
+      element2.backgroundColor = "blueviolet";
+    }
+  }
+}
+
+function insertion_sortonclick() {
+  start = window.performance.now();
+  (i = 1), (j = 0);
+  var elements = document.getElementById("array").children;
+  key_insertion = parseInt(elements[i].style.height);
+  elements[i].style.backgroundColor = "purple";
+  var startinterval = setInterval(insertionSort, 1000);
+  globinterval = startinterval;
 }
